b4534694567e2aa772c5faa7b3d84c33c91a89ac
juliaovod <ovoddev@gmail.com>
2022-06-14 23:09:42 +0300

    Step 2

--- src/cli.js
+++ src/cli.js
@@ -5,12 +5,11 @@ const program = new Command();
 program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
-  .version('0.0.1');
-
-program
-  .command('gendiff')
-  .option('-V, --version', 'output the version number')
-  .option('-h, --help', 'display help for command')
+  .version('0.0.1')
+  .argument('<filepath1>')
+  .argument('<filepath2>')
+  .helpOption('-h, --help', 'output usage information')
+  .option('-f, --format <type>', 'output format');
 
 program.parse();
 
