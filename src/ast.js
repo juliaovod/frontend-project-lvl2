e7c2787474c0e4519f511eff886e5fcff1d3d6ad
juliaovod <ovoddev@gmail.com>
2023-09-09 01:22:25 +0300

    Use map instead of for-of in buildAst

--- src/ast.js
+++ src/ast.js
@@ -11,36 +11,36 @@ const buildAst = (json1, json2) => {
   const iter = (obj1, obj2, accum, depth) => {
     const keys = _.sortBy(Object.keys({ ...obj1, ...obj2 }));
 
-    // eslint-disable-next-line no-restricted-syntax
-    for (const key of keys) {
+    return keys.map((key) => {
       const oldValue = obj1[key];
       const newValue = obj2[key];
 
-      const node = { key, depth };
+      const node = { key, level: depth };
 
       if (_.hasIn(obj1, key) && _.hasIn(obj2, key)) {
         if (_.isObject(oldValue) && _.isObject(newValue)) {
-          accum.push({
+          return {
             ...node,
-            children: iter(oldValue, newValue, [], depth + 1),
-          });
-        } else {
-          const status = oldValue !== newValue ? diffStatus.updated : diffStatus.unmodified;
-          accum.push({
-            ...node, oldValue, newValue, status,
-          });
+            children: iter(oldValue, newValue, depth + 1),
+          };
         }
-      } else if (_.hasIn(obj1, key)) {
-        accum.push({ ...node, oldValue, status: diffStatus.removed });
-      } else {
-        accum.push({ ...node, newValue, status: diffStatus.added });
-      }
-    }
 
-    return accum;
+        const status = oldValue !== newValue ? diffStatus.updated : diffStatus.unmodified;
+        return {
+          ...node,
+          oldValue,
+          newValue,
+          status,
+        };
+      }
+
+      return _.hasIn(obj1, key)
+        ? { ...node, oldValue, status: diffStatus.removed }
+        : { ...node, newValue, status: diffStatus.added };
+    });
   };
 
-  return iter(json1, json2, [], 1);
+  return iter(json1, json2, 1);
 };
 
 export default buildAst;
