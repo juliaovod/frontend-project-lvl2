dfe88f59322d8e5d09986fef771a683b2f113abb
juliaovod <ovoddev@gmail.com>
2023-09-10 14:39:06 +0300

    Return flatMap instead of reduce in plain.js

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -9,47 +9,40 @@ const stringify = (value) => {
   return _.isString(value) ? `'${value}'` : `${value}`;
 };
 
+const makeLine = (node, paths) => {
+  const { status, oldValue, newValue } = node;
+
+  const currentPaths = paths.join('.');
+  const value1 = stringify(oldValue);
+  const value2 = stringify(newValue);
+
+  switch (status) {
+    case diffStatus.updated:
+      return `Property '${currentPaths}' was updated. From ${value1} to ${value2}`;
+    case diffStatus.added:
+      return `Property '${currentPaths}' was added with value: ${value2}`;
+    case diffStatus.removed:
+      return `Property '${currentPaths}' was removed`;
+    default:
+      return null;
+  }
+};
+
 const plain = (diffAst) => {
-  const makeLine = (node, paths) => {
-    const { status, oldValue, newValue } = node;
-
-    const currentPaths = paths.join('.');
-    const value1 = stringify(oldValue);
-    const value2 = stringify(newValue);
-
-    switch (status) {
-      case diffStatus.updated:
-        return `Property '${currentPaths}' was updated. From ${value1} to ${value2}`;
-      case diffStatus.added:
-        return `Property '${currentPaths}' was added with value: ${value2}`;
-      case diffStatus.removed:
-        return `Property '${currentPaths}' was removed`;
-      default:
-        return null;
-    }
-  };
-
   const iter = (tree, depth, paths) => {
-    const lines = tree.reduce((accum, node) => {
+    const lines = tree.flatMap((node) => {
       const { key, children } = node;
 
       const currentPaths = [...paths, key];
 
       if (children) {
-        accum.push(iter(children, depth + 1, currentPaths));
-        return accum;
+        return iter(children, depth + 1, currentPaths);
       }
 
-      const line = makeLine(node, currentPaths);
-
-      if (line) {
-        accum.push(line);
-      }
-
-      return accum;
+      return makeLine(node, currentPaths);
     }, []);
 
-    return lines.join('\n');
+    return lines.filter(Boolean).join('\n');
   };
 
   return iter(diffAst, 1, []);
